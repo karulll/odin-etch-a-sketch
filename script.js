b0d7f967de2c8837d7cd2acd32a866385e9bfbb1
karulll <180303552+karulll@users.noreply.github.com>
2025-10-31 01:11:35 +0800

    Fix canvas selection method and ensure grid size updates correctly

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const canvas = document.getElementsByClassName("canvas");
+// start 
+const canvas = document.querySelector(".canvas");
 const inputGridValue = document.getElementById("grid-size");
 const gridValue = document.getElementById("grid-value"); 
 
@@ -8,5 +9,7 @@ function setCanvasSize(size){
     size = Math.round(Number(inputGridValue.value));
     gridValue.textContent = size + "x" + size;
 
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
 }
