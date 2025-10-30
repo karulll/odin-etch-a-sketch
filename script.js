b6d06b83494d4272131dde7b83732de0d37a1a6c
karulll <180303552+karulll@users.noreply.github.com>
2025-10-31 01:53:21 +0800

    Impement canvas grid creation and update default grid size to 16x16

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ const canvas = document.querySelector(".canvas");
 const inputGridValue = document.getElementById("grid-size");
 const gridValue = document.getElementById("grid-value"); 
 
-inputGridValue.addEventListener("input", setCanvasSize);
+let size = 16;
 
-function setCanvasSize(size){
+createGridCells(size);
+
+inputGridValue.addEventListener("input", setGridValue);
+
+function createGridCells(size) {
+    document.querySelectorAll('.cell').forEach(e => e.remove());
+    for(let i = 1; i <= size * size; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.id = `${i}`;
+        canvas.appendChild(cell);
+        console.log(i);
+    }
+}
+
+function setGridValue(size) {
     size = Math.round(Number(inputGridValue.value));
     gridValue.textContent = size + "x" + size;
 
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
+
+    createGridCells(size);
 }
