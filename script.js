fe6022eefd9ec364d9acc05007e931ab01ad452d
karulll <180303552+karulll@users.noreply.github.com>
2025-10-31 21:23:17 +0800

    Implement event listeners for mode selection adding .active to button class list

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 const canvas = document.querySelector(".canvas");
 const inputGridValue = document.getElementById("grid-size");
 const gridValue = document.getElementById("grid-value"); 
+const colorMode = document.getElementById("color-mode"); 
+const pickerMode = document.getElementById("picker-mode"); 
+const rainbowMode = document.getElementById("rainbow-mode"); 
+const shadingMode = document.getElementById("shading-mode"); 
+const borderToggle = document.getElementById("border-toggle"); 
+const clearCanvas = document.getElementById("clear-canvas"); 
+let currentActive = 0;
 
 let size = 16;
-
 createGridCells(size);
+
+colorMode.addEventListener("click", ()=> setActiveMode(colorMode.value));
+pickerMode.addEventListener("click", ()=> setActiveMode(pickerMode.value));
+rainbowMode.addEventListener("click", ()=> setActiveMode(rainbowMode.value));
+shadingMode.addEventListener("click", ()=> setActiveMode(shadingMode.value));
+borderToggle.addEventListener("click", ()=> setActiveMode(borderToggle.value));
+clearCanvas.addEventListener("click", ()=> setActiveMode(clearCanvas.value));
 
 inputGridValue.addEventListener("input", setGridValue);
 
@@ -28,4 +41,35 @@ function setGridValue(size) {
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     createGridCells(size);
+}
+
+function setActiveMode(value) {
+    if(value != 6) {
+        [   colorMode, pickerMode, 
+            rainbowMode, shadingMode, 
+            borderToggle, clearCanvas
+        ].forEach(e => e.classList.remove("active"));
+    }
+    
+    switch (value) {
+        case "1":
+            colorMode.classList.add("active");
+            break;
+        case "2":
+            pickerMode.classList.add("active");
+            break;
+        case "3":
+            rainbowMode.classList.add("active");
+            break;
+        case "4":
+            shadingMode.classList.add("active");
+            break;
+        case "5":
+            borderToggle.classList.add("active");
+            break;
+        case 6:
+            console.log("clear");
+            break;
+    }
+    console.log(value);
 }
